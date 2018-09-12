54d07bd634cbe899c6eaa454c730d48549f8490a
MechaDragonX <sonicraghav@gmail.com>
2018-09-12 16:40:41 -0700

    Added 9/3
    Added homweork from 9/3.

---
+++ Java/9-3HW/src/Problem14.java
@@ -0,0 +1,29 @@
+public class Problem14 {
+
+	public static void main(String[] args) 
+	{
+		problem12();
+		System.out.println();
+		
+		problem13();
+		System.out.println();
+	}
+	public static void problem12() {
+		
+		System.out.println("\"Several slashes are sometimes seen,\"");
+		System.out.println("said Sally, \"I've said said so.\" See?");
+		System.out.println("\\ / \\\\ // \\\\\\ ///");
+	}
+	public static void problem13 () {
+		
+		System.out.println("This is a test of your");
+		System.out.println("knowledge of \"quotes\" used");
+		System.out.println("in 'string literals.'");
+		
+		System.out.println();
+		
+		System.out.println("You're bound to \"get it right\"");
+		System.out.println("if you read the section on");
+		System.out.println("''quotes.''");
+	}
+}
---
+++ Java/MyFirstApp/src/myFirstApp.java
@@ -0,0 +1,26 @@
+public class myFirstApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+		System.out.println("I am in Mr. Parker's IB CS class!");
+		System.out.println("I used the 'sysout' shortcut!");
+		System.out.println();
+		
+		System.out.println("3+5 is "+(3+5)+".");
+		
+		System.out.println();
+		System.out.println("Below is a format print statement.");
+		System.out.println();
+		int x = 3 + 5;
+		System.out.printf("3+5 is %d.", x);
+		
+		/*
+		 * Why does this exist?
+		 * There is no reason for this?
+		 * Aaaaaaaaaaaaaaaauuuuuggggghhhhhhh!!!!!!!!!!
+		 */
+		
+	}
+
+}
